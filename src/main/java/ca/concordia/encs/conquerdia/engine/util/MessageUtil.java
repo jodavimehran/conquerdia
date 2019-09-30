322f4fe2aca078903be12ae2203caa041e89abf6
Mehran Jodavi <mehran.jodavi@gmail.com>
2019-09-30 19:39:32 -0300

    Add MessageUtil class

---
+++ src/main/java/ca/concordia/encs/conquerdia/engine/util/MessageUtil.java
@@ -0,0 +1,14 @@
+package ca.concordia.encs.conquerdia.engine.util;
+
+/**
+ * Util Class that should be useful to reuse static messages
+ */
+public final class MessageUtil {
+    public final static String EDIT_CONTINENT_COMMAND_ERR1 = "Invalid input! The \"editcontinent\" command must has at least one option like \"-add\" or \"-remove\".";
+
+    /**
+     * Private Constructor to avoid object creation
+     */
+    private MessageUtil() {
+    }
+}
