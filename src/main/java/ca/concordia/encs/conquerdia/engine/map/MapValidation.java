117458e31101158a8b2e26a8d215f0cdb6ea1a47
mosaliza <mosaliza@gmail.com>
2019-10-02 09:31:34 -0500

    #7  Implemented isEeachCountryBelongingToOnlyOneContinentTestCase() to validate graph
    This method is implemented to validate that a country belongs to one and only one continent.

--- src/main/java/ca/concordia/encs/conquerdia/engine/map/MapValidation.java
+++ src/main/java/ca/concordia/encs/conquerdia/engine/map/MapValidation.java
@@ -1,7 +1,9 @@
 package ca.concordia.encs.conquerdia.engine.map;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 /**
@@ -49,7 +51,7 @@ public class MapValidation {
 	}
 	/**
 	 * This Method verifies if the worldMap is a connected graph.
-	 * @param worldMap
+	 * @param worldMap the main grapgh of the game.
 	 * @return
 	 */
 	public boolean isMapAConnectedGraph(WorldMap worldMap) {
@@ -82,7 +84,33 @@ public class MapValidation {
 		}
 		return true;
 	}
-	
+	/**
+	 * This method Implements the validation rule that no country should belong to more than one continent on the map.
+	 * @param worldMap the main graph of the game.
+	 * @return true if there is no country with more than one continent; otherwise returns false.
+	 */
+	public boolean isEeachCountryBelongingToOnlyOneContinent(WorldMap worldMap) {
+		Set<Continent> continents  = worldMap.getContinents();
+		Map<Country , List<Continent>> mapCountriesAndContinents = new HashMap<Country, List<Continent>>();
+		for(Continent continent : continents) {
+			for(Country country : continent.getCountries()) {
+				if(mapCountriesAndContinents.containsKey(country)) {
+					mapCountriesAndContinents.get(country).add(country.getContinent());				
+				}else {
+					List<Continent> continentsList = new ArrayList<Continent>();
+					continentsList.add(country.getContinent());
+					mapCountriesAndContinents.put(country , continentsList);
+				}
+			}
+		}
+		//No Country should belong to more than one continent. 
+		for(Country country : mapCountriesAndContinents.keySet()) {
+			if(mapCountriesAndContinents.get(country).size() > 1) {
+				return false;
+			}
+		}
+		return true;
+	}	
 }
 
 
