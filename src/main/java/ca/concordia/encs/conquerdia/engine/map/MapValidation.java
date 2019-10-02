6bd4c5b37bfbb6a16fe3648af0cb93fbffddee24
mosaliza <mosaliza@gmail.com>
2019-10-01 20:54:23 -0500

    #7 : Implementing isMapAConnectedGraph in the MapValidation Class.
    This validation is one of  criterias of a valid worldMap graph.

--- src/main/java/ca/concordia/encs/conquerdia/engine/map/MapValidation.java
+++ src/main/java/ca/concordia/encs/conquerdia/engine/map/MapValidation.java
@@ -18,10 +18,10 @@ public class MapValidation {
 		this.continents = worldMap.getContinents();
 	}	
 	/**
-	 * 
+	 * This Method verifies if the continent is a connected subGraph of the worldMap.
 	 * @return true if the continent is a valid Subgraph of the WorldMap; otherwise returns false
 	 */
-	boolean isContinentAConnectedSubGraphOfWorldMap(Continent continent) {
+	public boolean isContinentAConnectedSubGraphOfWorldMap(Continent continent) {
 		
 		if(continent != null & continents.contains(continent)) {
 			if(continent.getCountries().size() > 0) {
@@ -47,4 +47,42 @@ public class MapValidation {
 			return false;
 		}
 	}
+	/**
+	 * This Method verifies if the worldMap is a connected graph.
+	 * @param worldMap
+	 * @return
+	 */
+	public boolean isMapAConnectedGraph(WorldMap worldMap) {
+		Set<Continent> disconnectedContinents = new HashSet<Continent>();
+		for(Continent continent : worldMap.getContinents()) {
+			Map<Country ,Set<Country>> mapAdjacentCountriesWithCountryAsKey = new HashMap<Country, Set<Country>>();		
+			Map<Country , Set<Country>> mapCountryWithAdjacentCountriesInAnotherContinent = new HashMap<Country, Set<Country>>();
+			for(Country country : continent.getCountries()) {
+				mapAdjacentCountriesWithCountryAsKey.put(country,country.getAdjacentCountries());
+			}
+			for(Country country : mapAdjacentCountriesWithCountryAsKey.keySet()) {
+				Set<Country> adjacentCountries = mapAdjacentCountriesWithCountryAsKey.get(country);
+				for(Country adjacentCountry : adjacentCountries) {
+					if(adjacentCountry.getContinent() != continent) {
+						Set<Country> adjacentCountriesinAnotherContinent = new HashSet<Country>();
+						adjacentCountriesinAnotherContinent.add(adjacentCountry);
+						mapCountryWithAdjacentCountriesInAnotherContinent.put(country, adjacentCountriesinAnotherContinent);
+					}
+				}
+			}
+			if(mapCountryWithAdjacentCountriesInAnotherContinent.keySet().size() == 0) {
+				disconnectedContinents.add(continent);
+			}
+		}
+		/* Map is a disconnected graph when there is at least one disconnected continent
+		 * otherwise it is validated as a connected graph.
+		 */
+		if(disconnectedContinents.size() > 0) {
+			return false;
+		}
+		return true;
+	}
+	
 }
+
+
