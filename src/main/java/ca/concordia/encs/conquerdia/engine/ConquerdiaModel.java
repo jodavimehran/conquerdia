b0d517a6fcd1cfcc35c778dc86278fe549009bd4
Mehran Jodavi <m_jodavi@encs.concordia.ca>
2019-10-14 18:16:29 -0300

    #25 Change Player Data Model From Set to HashMap

--- src/main/java/ca/concordia/encs/conquerdia/engine/ConquerdiaModel.java
+++ src/main/java/ca/concordia/encs/conquerdia/engine/ConquerdiaModel.java
@@ -3,11 +3,11 @@ package ca.concordia.encs.conquerdia.engine;
 import ca.concordia.encs.conquerdia.engine.map.WorldMap;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class ConquerdiaModel {
     private final WorldMap worldMap = new WorldMap();
-    private final HashSet<String> players = new HashSet<>();
+    private final HashMap<String, Player> players = new HashMap<>();
     private GamePhases currentPhase;
 
     /**
@@ -23,25 +23,32 @@ public class ConquerdiaModel {
     }
 
     /**
+     * Add a new player to the game if player name will not found in current player name is
+     *
      * @param playerName
      * @return
      */
     public String addPlayer(String playerName) {
         if (StringUtils.isBlank(playerName))
             return "Player name is not valid!";
-        if (players.contains(playerName))
+        if (players.containsKey(playerName))
             return String.format("Player with name \"%s\" is already exist.", playerName);
-        players.add(playerName);
+        players.put(playerName, new Player.Builder(playerName).build());
         return String.format("Player with name \"%s\" is successfully added.", playerName);
     }
 
+    /**
+     * @param playerName
+     * @return
+     */
     public String removePlayer(String playerName) {
-        if (!players.contains(playerName)) {
+        if (!players.containsKey(playerName)) {
             return String.format("Player with name \"%s\" is not found.", playerName);
         }
         players.remove(playerName);
         return String.format("Player with name \"%s\" is successfully removed.", playerName);
     }
+
 
     /**
      * This method gets the worldMap that contains all Continents and countries
