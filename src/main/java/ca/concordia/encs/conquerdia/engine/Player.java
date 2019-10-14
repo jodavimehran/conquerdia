2ce614f918cf2a1b7223dfff47d2827ba9372d3e
Mehran Jodavi <m_jodavi@encs.concordia.ca>
2019-10-14 18:09:47 -0300

    #25 Add Player Class

---
+++ src/main/java/ca/concordia/encs/conquerdia/engine/Player.java
@@ -0,0 +1,16 @@
+package ca.concordia.encs.conquerdia.engine;
+
+/**
+ * Represents a player in the Game
+ */
+public class Player {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
