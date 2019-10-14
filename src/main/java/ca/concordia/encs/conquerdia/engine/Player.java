8fcd62c9d05934eedfc2a0b20fa4e5e451f41da5
Mehran Jodavi <m_jodavi@encs.concordia.ca>
2019-10-14 18:13:42 -0300

    #25 Add PlayerBuilder

--- src/main/java/ca/concordia/encs/conquerdia/engine/Player.java
+++ src/main/java/ca/concordia/encs/conquerdia/engine/Player.java
@@ -4,13 +4,37 @@ package ca.concordia.encs.conquerdia.engine;
  * Represents a player in the Game
  */
 public class Player {
-    private String name;
+    private final String name;
+
+    private Player(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    /**
+     * The Builder for {@link Player}
+     */
+    public static class Builder {
+        private final Player player;
+
+        /**
+         * Player-Builder's constructor has one parameter because a player must have name.
+         *
+         * @param name The name of a player
+         */
+        public Builder(String name) {
+            player = new Player(name);
+        }
+
+
+        /**
+         * @return return built player
+         */
+        public Player build() {
+            return this.player;
+        }
     }
 }
