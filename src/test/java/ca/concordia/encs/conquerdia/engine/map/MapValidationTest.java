7b1e5616459ddb424e3ab626c09d080f2be87a50
shiblu <mkshiblu@gmail.com>
2019-10-16 20:31:25 -0400

    #36: Fix map writer code

--- src/test/java/ca/concordia/encs/conquerdia/engine/map/MapValidationTest.java
+++ src/test/java/ca/concordia/encs/conquerdia/engine/map/MapValidationTest.java
@@ -30,16 +30,16 @@ public class MapValidationTest {
 
         //Build sample countries in Asia and determine their adjacent countries
         worldMap.addCountry("Iran", "Asia");
-        worldMap.addCountry("Saudi Arabia", "Asia");
+        worldMap.addCountry("SaudiArabia", "Asia");
         worldMap.addCountry("Armenia", "Asia");
         worldMap.addCountry("Turkey", "Asia");
         worldMap.addCountry("Greece", "Europe");
 
 
-        worldMap.addNeighbour("Iran", "Saudi Arabia");
+        worldMap.addNeighbour("Iran", "SaudiArabia");
         worldMap.addNeighbour("Iran", "Armenia");
         worldMap.addNeighbour("Iran", "Turkey");
-        worldMap.addNeighbour("Saudi Arabia", "Turkey");
+        worldMap.addNeighbour("SaudiArabia", "Turkey");
         worldMap.addNeighbour("Greece", "Turkey");
 
         mapValidation = new MapValidation(worldMap);
--- src/test/java/ca/concordia/encs/conquerdia/engine/map/io/MapWriterTest.java
+++ src/test/java/ca/concordia/encs/conquerdia/engine/map/io/MapWriterTest.java
@@ -1,15 +1,21 @@
 package ca.concordia.encs.conquerdia.engine.map.io;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
+import org.junit.Test;
 
 import ca.concordia.encs.conquerdia.engine.map.WorldMap;
 
 public class MapWriterTest {
 	private WorldMap worldMap;
+	private IMapWriter writer;
+	private boolean isWriteSuccessful;
 
 	@Before
 	public void beforeTests() {
 		worldMap = new WorldMap();
+		worldMap.editMap("test");
 
 		// CONTINENTS
 		worldMap.addContinent("Asia", 1);
@@ -17,15 +23,23 @@ public class MapWriterTest {
 
 		// Build sample countries in Asia and determine their adjacent countries
 		worldMap.addCountry("Iran", "Asia");
-		worldMap.addCountry("Saudi Arabia", "Asia");
+		worldMap.addCountry("Saudi-Arabia", "Asia");
 		worldMap.addCountry("Armenia", "Asia");
 		worldMap.addCountry("Turkey", "Asia");
 		worldMap.addCountry("Greece", "Europe");
 
-		worldMap.addNeighbour("Iran", "Saudi Arabia");
+		worldMap.addNeighbour("Iran", "Saudi-Arabia");
 		worldMap.addNeighbour("Iran", "Armenia");
 		worldMap.addNeighbour("Iran", "Turkey");
-		worldMap.addNeighbour("Saudi Arabia", "Turkey");
+		worldMap.addNeighbour("Saudi-Arabia", "Turkey");
 		worldMap.addNeighbour("Greece", "Turkey");
+
+		writer = new MapWriter(worldMap);
+		isWriteSuccessful = writer.writeMap("test");
+	}
+
+	@Test
+	public void testWriteMap() {
+		assertTrue(isWriteSuccessful);
 	}
 }
