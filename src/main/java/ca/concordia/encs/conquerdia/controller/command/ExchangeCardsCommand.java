e9b096c3c807c854ebdfe2cac9d901128cf88620
Mehran Jodavi <m_jodavi@encs.concordia.ca>
2019-11-04 20:38:31 -0400

    #45

---
+++ src/main/java/ca/concordia/encs/conquerdia/controller/command/ExchangeCardsCommand.java
@@ -0,0 +1,33 @@
+package ca.concordia.encs.conquerdia.controller.command;
+
+import ca.concordia.encs.conquerdia.exception.ValidationException;
+import ca.concordia.encs.conquerdia.model.PhaseModel;
+
+import java.util.List;
+
+public class ExchangeCardsCommand extends AbstractCommand {
+    public final static String COMMAND_HELP_MSG = "exchangecards num num num –none (exchange three cards from the hand, as specified by three numbers that represent the position of the exchanged cards in the player’s hand. If –none is specified, choose to not exchange cards.)";
+
+    @Override
+    protected void runCommand(List<String> inputCommandParts) throws ValidationException {
+        if ("–none".equals(inputCommandParts.get(1))) {
+            return;
+        } else {
+            try {
+                phaseLogList.add(PhaseModel.getInstance().getCurrentPlayer().exchangeCard(Integer.valueOf(inputCommandParts.get(1)), Integer.valueOf(inputCommandParts.get(2)), Integer.valueOf(inputCommandParts.get(3))));
+            } catch (NumberFormatException ex) {
+                throw new ValidationException("Card number must be an integer.");
+            }
+        }
+    }
+
+    @Override
+    protected String getCommandHelpMessage() {
+        return COMMAND_HELP_MSG;
+    }
+
+    @Override
+    protected CommandType getCommandType() {
+        return CommandType.EXCHANGE_CARDS;
+    }
+}
