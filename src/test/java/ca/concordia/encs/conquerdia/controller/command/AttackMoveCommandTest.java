23d9d75f477408e31d50cf4d6a03d6b937acdf65
shiblu <mosabbir.shiblu@gmail.com>
2019-11-04 21:50:04 -0500

    [Attack] Add simple test for attack move command

---
+++ src/test/java/ca/concordia/encs/conquerdia/controller/command/AttackMoveCommandTest.java
@@ -0,0 +1,27 @@
+package ca.concordia.encs.conquerdia.controller.command;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import ca.concordia.encs.conquerdia.exception.ValidationException;
+
+public class AttackMoveCommandTest extends AttackPhaseCommandTest {
+
+	@Test
+	public void testCommandValidity() {
+
+		DefendCommand defendCommand = new DefendCommand();
+		List<String> list = new ArrayList<String>();
+		list.add("attackmove");
+		list.add("-3");
+		String message = null;
+		try {
+			defendCommand.runCommand(list);
+		} catch (ValidationException ex) {
+			message = ex.getMessage();
+		}
+		assertTrue(message.contains("must be a positive integer"));
+	}
+}
