ceee6df5a79977501d064d1256a52021fa61d651
mosaliza <mosaliza@gmail.com>
2019-11-06 15:08:19 -0500

    Creating TestSuite for Game File io

---
+++ src/test/java/ca/concordia/encs/conquerdia/model/map/io/FileTestSuite.java
@@ -0,0 +1,15 @@
+package ca.concordia.encs.conquerdia.model.map.io;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	CardReaderTest.class,
+	FileHelperTest.class,
+	MapReaderTest.class,
+	MapWriterTest.class
+})
+public class FileTestSuite {
+
+}
