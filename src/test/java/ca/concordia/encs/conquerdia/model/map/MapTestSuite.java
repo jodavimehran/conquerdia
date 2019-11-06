1e04c42460da1d423fd8ee5e2be27fecd3873fed
mosaliza <mosaliza@gmail.com>
2019-11-06 15:03:40 -0500

    Creating TestSuite for Map

---
+++ src/test/java/ca/concordia/encs/conquerdia/model/map/MapTestSuite.java
@@ -0,0 +1,15 @@
+package ca.concordia.encs.conquerdia.model.map;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	CountryTest.class,
+	MapFormattorTest.class,
+	MapValidationTest.class
+})
+public class MapTestSuite {
+
+}
