5e6f4ea6cb804b97c5958d37eb63721e08a84d83
mosaliza <mosaliza@gmail.com>
2019-11-06 14:48:08 -0500

    Creating TestSuite for PlayerTest

---
+++ src/test/java/ca/concordia/encs/conquerdia/model/PlayerTestSuite.java
@@ -0,0 +1,13 @@
+package ca.concordia.encs.conquerdia.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	PlayerTest.class
+})
+public class PlayerTestSuite {
+
+}
