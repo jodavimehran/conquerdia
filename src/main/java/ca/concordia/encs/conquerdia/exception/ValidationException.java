8f9c8f2faa6743b93110108cfc54032653c7e3d0
mosaliza <mosaliza@gmail.com>
2019-11-07 11:48:42 -0500

    Adding JavaDoc for ValidationExeption class

--- src/main/java/ca/concordia/encs/conquerdia/exception/ValidationException.java
+++ src/main/java/ca/concordia/encs/conquerdia/exception/ValidationException.java
@@ -6,16 +6,25 @@ import java.util.List;
 public class ValidationException extends Exception {
     private final List<String> validationErrors = new ArrayList<>();
 
-
+    /**
+     * The constructor for ValidateException Class
+     * @param message the string that shows the Validation error.
+     */
     public ValidationException(String message) {
         super(message);
         validationErrors.add("Validation Error! " + message);
     }
-
+    /**
+     * Adds a new validation Error to the String list of Validation Errors
+     * @param error the string that shows the Validation error.
+     */
     public void addValidationError(String error) {
         validationErrors.add("Validation Error! " + error);
     }
-
+    /**
+     * Gets the validation Errors
+     * @return returns the current String List of Validation Errors
+     */
     public List<String> getValidationErrors() {
         return validationErrors;
     }
