902b972fd801857568003a884f884305cb3fa4f2
shiblu <mosabbir.shiblu@gmail.com>
2019-11-29 13:09:58 -0500

    Finish javadocs for tests

--- src/test/java/ca/concordia/encs/conquerdia/model/AbstractPlayerTest.java
+++ src/test/java/ca/concordia/encs/conquerdia/model/AbstractPlayerTest.java
@@ -11,127 +11,154 @@ import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
+/**
+ * Tests for players
+ */
 public class AbstractPlayerTest {
-    private final static String PLAYER_NAME = "testPlayer";
-    private Player player;
-    private Continent testContinent;
-    private Country country1;
-    private Country country2;
-    private Country country3;
 
-    /**
-     * All common activities are placed here
-     */
-    @Before
-    public void setUp() {
+	/**
+	 * Name of the player
+	 */
+	private final static String PLAYER_NAME = "testPlayer";
 
-        try {
-            player = Player.factory(PLAYER_NAME, "human");
-        } catch (ValidationException e) {
-            e.printStackTrace();
-        }
-        testContinent = new Continent.Builder("testContinent").setValue(8).build();
-        country1 = new Country.Builder("one", testContinent).build();
-        country2 = new Country.Builder("two", testContinent).build();
-        country3 = new Country.Builder("tree", testContinent).build();
+	/**
+	 * Player object
+	 */
+	private Player player;
 
-        player.addCountry(country1);
-        player.addCountry(country2);
-        player.addCountry(country3);
+	/**
+	 * Continent to test
+	 */
+	private Continent testContinent;
 
+	/**
+	 * Country to test
+	 */
+	private Country country1;
 
-    }
+	/**
+	 * Country2
+	 */
+	private Country country2;
 
-    /**
-     * Test case for {@link Player#getName() getName} method
-     */
-    @Test
-    public void getName() {
-        assertEquals(player.getName(), PLAYER_NAME);
-    }
+	/**
+	 * Country3
+	 */
+	private Country country3;
 
-    /**
-     * Test case for {@link Player#addUnplacedArmies(int)} () addUnplacedArmies} method
-     */
-    @Test
-    public void addUnplacedArmies() {
-        player.addUnplacedArmies(100);
-        assertEquals(100, player.getUnplacedArmies());
-    }
+	/**
+	 * All common activities are placed here
+	 */
+	@Before
+	public void setUp() {
 
-    /**
-     * Test case for {@link Player#minusUnplacedArmies(int)} method
-     */
-    @Test
-    public void minusUnplacedArmies() {
-        player.addUnplacedArmies(100);
-        player.minusUnplacedArmies(50);
-        assertEquals(50, player.getUnplacedArmies());
-    }
+		try {
+			player = Player.factory(PLAYER_NAME, "human");
+		} catch (ValidationException e) {
+			e.printStackTrace();
+		}
+		testContinent = new Continent.Builder("testContinent").setValue(8).build();
+		country1 = new Country.Builder("one", testContinent).build();
+		country2 = new Country.Builder("two", testContinent).build();
+		country3 = new Country.Builder("tree", testContinent).build();
 
-    /**
-     * Test case for {@link Player#addCountry(Country)} method
-     */
-    @Test
-    public void addCountry() {
-        Country country4 = new Country.Builder("four", testContinent).build();
-        player.addCountry(country4);
-        assertTrue(player.getCountryNames().contains("four"));
-    }
+		player.addCountry(country1);
+		player.addCountry(country2);
+		player.addCountry(country3);
 
-    /**
-     * Test case for {@link Player#removeCountry(String)}  method
-     */
-    @Test
-    public void removeCountry() {
-        player.removeCountry(country3.getName());
-        assertFalse(player.getCountryNames().contains("tree"));
-    }
+	}
 
-    /**
-     * Test case for {@link Player#getNumberOfCountries()}  method
-     */
-    @Test
-    public void getNumberOfCountries() {
-        assertEquals(3, player.getNumberOfCountries());
-    }
+	/**
+	 * Test case for {@link Player#getName() getName} method
+	 */
+	@Test
+	public void getName() {
+		assertEquals(player.getName(), PLAYER_NAME);
+	}
 
-    /**
-     * Test case for {@link Player#getCountryNames()} addUnplacedArmies} method
-     */
-    @Test
-    public void getCountryNames() {
-        assertTrue(player.getCountryNames().containsAll(Arrays.asList("one", "two", "tree")));
-    }
+	/**
+	 * Test case for {@link Player#addUnplacedArmies(int)} () addUnplacedArmies}
+	 * method
+	 */
+	@Test
+	public void addUnplacedArmies() {
+		player.addUnplacedArmies(100);
+		assertEquals(100, player.getUnplacedArmies());
+	}
 
-    /**
-     * Test case for {@link Player#getNumberOfContinents()} ()} method
-     */
-    @Test
-    public void getNumberOfContinent() {
-        assertEquals(1, player.getNumberOfContinents());
-    }
+	/**
+	 * Test case for {@link Player#minusUnplacedArmies(int)} method
+	 */
+	@Test
+	public void minusUnplacedArmies() {
+		player.addUnplacedArmies(100);
+		player.minusUnplacedArmies(50);
+		assertEquals(50, player.getUnplacedArmies());
+	}
 
+	/**
+	 * Test case for {@link Player#addCountry(Country)} method
+	 */
+	@Test
+	public void addCountry() {
+		Country country4 = new Country.Builder("four", testContinent).build();
+		player.addCountry(country4);
+		assertTrue(player.getCountryNames().contains("four"));
+	}
 
-    /**
-     * Test case for {@link Player#calculateNumberOfReinforcementArmies() calculateNumberOfReinforcementArmies} method
-     */
-    @Test
-    public void calculateNumberOfReinforcementArmies() {
-        assertEquals(9, player.calculateNumberOfReinforcementArmies());
-        for (int i = 0; i < 10; i++) {
-            Country country = new Country.Builder("country" + i, testContinent).build();
-            player.addCountry(country);
-        }
-        assertEquals(12, player.calculateNumberOfReinforcementArmies());
-    }
+	/**
+	 * Test case for {@link Player#removeCountry(String)} method
+	 */
+	@Test
+	public void removeCountry() {
+		player.removeCountry(country3.getName());
+		assertFalse(player.getCountryNames().contains("tree"));
+	}
 
-    /**
-     * Test case for {@link Player#getUnplacedArmies() addUnplacedArmies} method
-     */
-    @Test
-    public void getUnplacedArmies() {
-        player.addUnplacedArmies(100);
-        assertEquals(100, player.getUnplacedArmies());
-    }
+	/**
+	 * Test case for {@link Player#getNumberOfCountries()} method
+	 */
+	@Test
+	public void getNumberOfCountries() {
+		assertEquals(3, player.getNumberOfCountries());
+	}
+
+	/**
+	 * Test case for {@link Player#getCountryNames()} addUnplacedArmies} method
+	 */
+	@Test
+	public void getCountryNames() {
+		assertTrue(player.getCountryNames().containsAll(Arrays.asList("one", "two", "tree")));
+	}
+
+	/**
+	 * Test case for {@link Player#getNumberOfContinents()} ()} method
+	 */
+	@Test
+	public void getNumberOfContinent() {
+		assertEquals(1, player.getNumberOfContinents());
+	}
+
+	/**
+	 * Test case for {@link Player#calculateNumberOfReinforcementArmies()
+	 * calculateNumberOfReinforcementArmies} method
+	 */
+	@Test
+	public void calculateNumberOfReinforcementArmies() {
+		assertEquals(9, player.calculateNumberOfReinforcementArmies());
+		for (int i = 0; i < 10; i++) {
+			Country country = new Country.Builder("country" + i, testContinent).build();
+			player.addCountry(country);
+		}
+		assertEquals(12, player.calculateNumberOfReinforcementArmies());
+	}
+
+	/**
+	 * Test case for {@link Player#getUnplacedArmies() addUnplacedArmies} method
+	 */
+	@Test
+	public void getUnplacedArmies() {
+		player.addUnplacedArmies(100);
+		assertEquals(100, player.getUnplacedArmies());
+	}
 }
--- src/test/java/ca/concordia/encs/conquerdia/model/map/MapFormattorTest.java
+++ src/test/java/ca/concordia/encs/conquerdia/model/map/MapFormattorTest.java
@@ -8,80 +8,88 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Test for map formattor
+ * 
+ * @author Mosabbir
+ *
+ */
 public class MapFormattorTest {
 
-    private static WorldMap map;
+	/**
+	 * Map to format
+	 */
+	private static WorldMap map;
 
-    /**
-     * All common activities are placed here
-     */
-    @BeforeClass
-    public static void setUpOnce() throws ValidationException {
-        map = WorldMap.getInstance();
+	/**
+	 * All common activities are placed here
+	 */
+	@BeforeClass
+	public static void setUpOnce() throws ValidationException {
+		map = WorldMap.getInstance();
 
-        map.addContinent("South-West-England", 5);
-        map.addCountry("Ross-shire", "South-West-England");
-        map.addCountry("Ross-shire-and-Chromartyshire", "South-West-England");
-        map.addCountry("Kincardine-shire", "South-West-England");
-        map.addCountry("Northamptonshire_Northamptonshire", "South-West-England");
+		map.addContinent("South-West-England", 5);
+		map.addCountry("Ross-shire", "South-West-England");
+		map.addCountry("Ross-shire-and-Chromartyshire", "South-West-England");
+		map.addCountry("Kincardine-shire", "South-West-England");
+		map.addCountry("Northamptonshire_Northamptonshire", "South-West-England");
 
-        map.addNeighbour("Ross-shire", "Ross-shire-and-Chromartyshire");
-        map.addNeighbour("Ross-shire", "Kincardine-shire");
-        map.addNeighbour("Ross-shire", "Northamptonshire_Northamptonshire");
+		map.addNeighbour("Ross-shire", "Ross-shire-and-Chromartyshire");
+		map.addNeighbour("Ross-shire", "Kincardine-shire");
+		map.addNeighbour("Ross-shire", "Northamptonshire_Northamptonshire");
 
-        map.getCountry("Ross-shire").placeArmy(5);
-        map.getCountry("Ross-shire-and-Chromartyshire").placeArmy(1);
-        map.getCountry("Kincardine-shire").placeArmy(4);
-        map.getCountry("Northamptonshire_Northamptonshire").placeArmy(15);
+		map.getCountry("Ross-shire").placeArmy(5);
+		map.getCountry("Ross-shire-and-Chromartyshire").placeArmy(1);
+		map.getCountry("Kincardine-shire").placeArmy(4);
+		map.getCountry("Northamptonshire_Northamptonshire").placeArmy(15);
 
-        Player p1 = Player.factory("John", "human");
-        map.getCountry("Ross-shire").setOwner(p1);
-        map.getCountry("Northamptonshire_Northamptonshire").setOwner(p1);
+		Player p1 = Player.factory("John", "human");
+		map.getCountry("Ross-shire").setOwner(p1);
+		map.getCountry("Northamptonshire_Northamptonshire").setOwner(p1);
 
-        map.getCountry("Kincardine-shire").setOwner(Player.factory("Robert", "human"));
-        map.getCountry("Ross-shire-and-Chromartyshire").setOwner(Player.factory("Doe", "human"));
+		map.getCountry("Kincardine-shire").setOwner(Player.factory("Robert", "human"));
+		map.getCountry("Ross-shire-and-Chromartyshire").setOwner(Player.factory("Doe", "human"));
 
-    }
+	}
 
-    /**
-     * Release all objects after All Test cases are performed in the Test Class
-     */
-    @AfterClass
-    public static void cleanup() {
-        WorldMap.getInstance().clear();
-    }
+	/**
+	 * Release all objects after All Test cases are performed in the Test Class
+	 */
+	@AfterClass
+	public static void cleanup() {
+		WorldMap.getInstance().clear();
+	}
 
-    /**
-     * TEst for Default Map formatter
-     */
-    @Test
-    public void testDefaultMapFormattor() {
-        MapFormattor formattor = new MapFormattor(map);
-        String res = formattor.format();
-        assertTrue(res.contains("Northamptonshire_Northamptonshire"));
-    }
+	/**
+	 * TEst for Default Map formatter
+	 */
+	@Test
+	public void testDefaultMapFormattor() {
+		MapFormattor formattor = new MapFormattor(map);
+		String res = formattor.format();
+		assertTrue(res.contains("Northamptonshire_Northamptonshire"));
+	}
 
-    /**
-     * Test for DetailMapFormatter
-     */
-    @Test
-    public void testDetailMapFormattor() {
-        MapFormattor formattor = new MapFormattor(map);
-        String res = formattor.format(MapFormattor.FormatType.Detail);
-        assertTrue(res.contains("Northamptonshire_Northamptonshire| 15    | John"));
-    }
+	/**
+	 * Test for DetailMapFormatter
+	 */
+	@Test
+	public void testDetailMapFormattor() {
+		MapFormattor formattor = new MapFormattor(map);
+		String res = formattor.format(MapFormattor.FormatType.Detail);
+		assertTrue(res.contains("Northamptonshire_Northamptonshire| 15    | John"));
+	}
 
-    /**
-     * Tests for PresenseOfEmptyContinents()
-     *
-     * @throws ValidationException
-     */
-    @Test
-    public void testPresenseOfEmptyContinents() throws ValidationException {
-        map.addContinent("Antarctica", 8);
-        MapFormattor formattor = new MapFormattor(map);
-        String res = formattor.format(MapFormattor.FormatType.Detail);
-        assertTrue(res.contains("Antarctica"));
-    }
+	/**
+	 * Tests for PresenseOfEmptyContinents()
+	 *
+	 * @throws ValidationException
+	 */
+	@Test
+	public void testPresenseOfEmptyContinents() throws ValidationException {
+		map.addContinent("Antarctica", 8);
+		MapFormattor formattor = new MapFormattor(map);
+		String res = formattor.format(MapFormattor.FormatType.Detail);
+		assertTrue(res.contains("Antarctica"));
+	}
 }
-
