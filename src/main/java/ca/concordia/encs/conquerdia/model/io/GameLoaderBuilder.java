9a949c594c19ac17c3c8124f9651b26941d7ba0e
mosaliza <mosaliza@gmail.com>
2019-11-29 12:25:22 -0500

    fix for adding the phaselog after loading the gamestate

--- src/main/java/ca/concordia/encs/conquerdia/model/io/GameLoaderBuilder.java
+++ src/main/java/ca/concordia/encs/conquerdia/model/io/GameLoaderBuilder.java
@@ -79,7 +79,7 @@ public class GameLoaderBuilder extends GameStateBuilder {
 		stateProduct.getPhaseModel().setAllCountriesArePopulated(allCountriesArePopulated);
 		//3.Updating the PlayersModel with the built state
 		PhaseModel.getInstance().setCurrentPhase(stateProduct.getPhaseModel().getCurrentPhase());
-		PhaseModel.getInstance().getPhaseLog().add("\n");
+		PhaseModel.getInstance().getPhaseLog().clear();
 		PhaseModel.getInstance().getPhaseLog().addAll(stateProduct.getPhaseModel().getPhaseLog());
 	}
 	/**
